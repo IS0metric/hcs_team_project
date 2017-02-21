4cf3016f47632fd2899ccfb12856f9cab31e5ccd
IS0metric <swgbhero@ymail.com>
2017-02-21 14:18:54 +0000

    Initial Framework

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# hcs_team_project
+# hcs_team_project #
 Repository for code relating to the technical aspect of Human Centered Security coursework.
+
+#### To run the app: ####
+1. Navigate to the directory containing `index.html` using the terminal
+2. Run the Python command: `python -m SimpleHTTPServer 8000`
+3. In a web browser, navigate to: <http://localhost:8000/>
+
+**Note for Developers:** once you have made changes you can view them by refreshing the browser window with <kbd>CTRL</kbd>+<kbd>F5</kbd>
---
+++ static/script.js
@@ -0,0 +1,40 @@
+var app = angular.module('app', ['ngRoute']);
+
+/** Routing Configuration
+ *  Whenever a specified route is accessed, the HTML is loaded into the main
+ *  view (located in the index page) and the controller loads the content
+ */
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+        templateUrl : 'templates/checker.html',
+        controller  : 'checker_controller'
+    })
+    .when('/success', {
+        templateUrl : 'templates/success.html',
+        controller  : 'success_controller'
+    });
+});
+
+/** Main View Controller
+ *  Maps the the app's main view, which includes the password checker
+ */
+app.controller('checker_controller', function($scope, $location) {
+  /* Will handle the core of the visual representation, including the random
+  characters and symbols, etc */
+  $scope.test_message = "This is where the password checker will be located";
+
+  $scope.check_password = function() {
+    // Will handle the core of the password checking and route to next page
+    var password = $scope.password;
+    console.log(password);
+    $location.path("/success");
+  }
+})
+
+/** Success Controller
+ *  Maps to the success page
+ */
+app.controller('success_controller', function($scope, $location) {
+  $scope.success_message = "Successful Registration"
+})
