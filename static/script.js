2760a7d271fda2540e95bd910d4236062ccf68bc
IS0metric <swgbhero@ymail.com>
2017-03-06 16:14:01 +0000

    Panels will change according to characters in input

--- static/script.js
+++ static/script.js
@@ -28,7 +28,6 @@ app.directive('update_password', function(){
 app.controller('checker_controller', function($scope, $location) {
   /* Will handle the core of the visual representation, including the random
   characters and symbols, etc */
-  $scope.test_message = ""; // To be removed
 
   $scope.has_symbol = false;
   $scope.has_lower_case = false;
@@ -51,6 +50,57 @@ app.controller('checker_controller', function($scope, $location) {
   console.log($scope.random_upper_case);
   console.log($scope.random_lower_case);
   console.log($scope.random_symbol);
+
+  $scope.change_password = function() {
+    password = $scope.password;
+
+    has_lower_case = false;
+    has_upper_case = false;
+    has_number = false;
+    has_symbol = false;
+
+    for (var i in lower_cases) {
+      if (password.indexOf(lower_cases[i]) > -1) {
+        has_lower_case = true;
+      }
+    }
+    for (var i in upper_cases) {
+      if (password.indexOf(upper_cases[i]) > -1) {
+        has_upper_case = true;
+      }
+    }
+    for (var i in symbols) {
+      if (password.indexOf(symbols[i]) > -1) {
+        has_symbol = true;
+      }
+    }
+    for (var i in numbers) {
+      if (password.indexOf(numbers[i]) > -1) {
+        has_number = true;
+      }
+    }
+
+    if (has_lower_case == true){
+      $("#lower_case_panel").addClass("success-panel");
+    } else {
+      $("#lower_case_panel").removeClass("success-panel");
+    }
+    if (has_upper_case == true){
+      $("#upper_case_panel").addClass("success-panel");
+    } else {
+      $("#upper_case_panel").removeClass("success-panel");
+    }
+    if (has_number == true){
+      $("#number_panel").addClass("success-panel");
+    } else {
+      $("#number_panel").removeClass("success-panel");
+    }
+    if (has_symbol == true){
+      $("#symbol_panel").addClass("success-panel");
+    } else {
+      $("#symbol_panel").removeClass("success-panel");
+    }
+  }
 
   $scope.check_password = function() {
     // Will handle the core of the password checking and route to next page
