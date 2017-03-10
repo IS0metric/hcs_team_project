f8f2dfe0cefad1c13369388b993631cc5343691a
IS0metric <swgbhero@ymail.com>
2017-03-10 15:50:35 +0000

    Added JavaScript to check password length and matching passwords, but HTML it refers to does not yet exist

--- static/script.js
+++ static/script.js
@@ -29,10 +29,10 @@ app.controller('checker_controller', function($scope, $location) {
   /* Will handle the core of the visual representation, including the random
   characters and symbols, etc */
 
-  $scope.has_symbol = false;
-  $scope.has_lower_case = false;
-  $scope.has_upper_case = false;
-  $scope.has_number = false;
+  var has_symbol = false;
+  var has_lower_case = false;
+  var has_upper_case = false;
+  var has_number = false;
 
   var symbols = "<>@!#$%^&*()_+[]{}?:;|'\"\\,./~`-=";
   $scope.random_symbol = symbols[Math.floor(Math.random() * symbols.length)];
@@ -46,6 +46,8 @@ app.controller('checker_controller', function($scope, $location) {
   var numbers = "1234567890";
   $scope.random_number = numbers[Math.floor(Math.random() * numbers.length)];
 
+
+
   console.log($scope.random_number);
   console.log($scope.random_upper_case);
   console.log($scope.random_lower_case);
@@ -54,10 +56,10 @@ app.controller('checker_controller', function($scope, $location) {
   $scope.change_password = function() {
     password = $scope.password;
 
+    has_symbol = false;
     has_lower_case = false;
     has_upper_case = false;
     has_number = false;
-    has_symbol = false;
 
     for (var i in lower_cases) {
       if (password.indexOf(lower_cases[i]) > -1) {
@@ -81,22 +83,22 @@ app.controller('checker_controller', function($scope, $location) {
     }
 
     if (has_lower_case == true){
-      $("#lower_case_panel").addClass("success-panel");
+      $("#lower_case_panel").removeClass("failure-panel").addClass("success-panel");
     } else {
       $("#lower_case_panel").removeClass("success-panel");
     }
     if (has_upper_case == true){
-      $("#upper_case_panel").addClass("success-panel");
+      $("#upper_case_panel").removeClass("failure-panel").addClass("success-panel");
     } else {
       $("#upper_case_panel").removeClass("success-panel");
     }
     if (has_number == true){
-      $("#number_panel").addClass("success-panel");
+      $("#number_panel").removeClass("failure-panel").addClass("success-panel");
     } else {
       $("#number_panel").removeClass("success-panel");
     }
     if (has_symbol == true){
-      $("#symbol_panel").addClass("success-panel");
+      $("#symbol_panel").removeClass("failure-panel").addClass("success-panel");
     } else {
       $("#symbol_panel").removeClass("success-panel");
     }
@@ -105,8 +107,26 @@ app.controller('checker_controller', function($scope, $location) {
   $scope.check_password = function() {
     // Will handle the core of the password checking and route to next page
     var password = $scope.password;
-    console.log(password);
-    $location.path("/success");
+    var pass = true;
+    if (has_lower_case != true){
+      pass = false;
+      $("#lower_case_panel").addClass("failure-panel");
+    }
+    if (has_upper_case != true){
+      pass = false;
+      $("#upper_case_panel").addClass("failure-panel");
+    }
+    if (has_number != true){
+      pass = false;
+      $("#number_panel").addClass("failure-panel");
+    }
+    if (has_symbol != true){
+      pass = false;
+      $("#symbol_panel").addClass("failure-panel");
+    }
+    if (pass == true){
+      $location.path("/success");
+    }
   }
 })
 
