e7ceda58013b4520c16935cc17abc6736c9dc748
IS0metric <swgbhero@ymail.com>
2017-03-10 17:22:00 +0000

    Cleaned up JavaScript

--- static/script.js
+++ static/script.js
@@ -45,9 +45,30 @@ app.controller('checker_controller', function($scope, $location) {
 
   var min_length = 8;
 
+
+
+  function check_character(password, character_list) {
+    var boolean = false;
+    for (var i in character_list) {
+      if (password.indexOf(character_list[i]) > -1) {
+        boolean = true;
+      }
+    }
+    return boolean;
+  }
+
+  function change_class(panel_name, boolean) {
+    if (boolean == true) {
+      $(panel_name).removeClass("failure-panel").addClass("success-panel");
+    } else {
+      $(panel_name).removeClass("success-panel");
+    }
+  }
+
   $scope.change_password = function() {
+    //
     var password = $scope.password;
-    var password_copy = $scope.password_copy
+    var password_copy = $scope.password_copy;
 
     has_symbol = false;
     has_lower_case = false;
@@ -56,62 +77,38 @@ app.controller('checker_controller', function($scope, $location) {
     is_long_enough = false;
     passwords_match = false;
 
-    for (var i in lower_cases) {
-      if (password.indexOf(lower_cases[i]) > -1) {
-        has_lower_case = true;
-      }
-    }
-    for (var i in upper_cases) {
-      if (password.indexOf(upper_cases[i]) > -1) {
-        has_upper_case = true;
-      }
-    }
-    for (var i in symbols) {
-      if (password.indexOf(symbols[i]) > -1) {
-        has_symbol = true;
-      }
-    }
-    for (var i in numbers) {
-      if (password.indexOf(numbers[i]) > -1) {
-        has_number = true;
-      }
-    }
+    // Check if the password has the required characters
+    has_lower_case = check_character(password, lower_cases);
+    has_upper_case = check_character(password, upper_cases);
+    has_number = check_character(password, numbers);
+    has_symbol = check_character(password, symbols);
+    // Check if the password is long enough
     if (password.length >= min_length) {
       is_long_enough = true;
     }
-    if (password != password_copy) {
+    // Check if the two passwords match
+    if (password == password_copy) {
       passwords_match = true;
     }
 
-    if (has_lower_case == true) {
-      $("#lower_case_panel").removeClass("failure-panel").addClass("success-panel");
-    } else {
-      $("#lower_case_panel").removeClass("success-panel");
-    }
-    if (has_upper_case == true) {
-      $("#upper_case_panel").removeClass("failure-panel").addClass("success-panel");
-    } else {
-      $("#upper_case_panel").removeClass("success-panel");
-    }
-    if (has_number == true) {
-      $("#number_panel").removeClass("failure-panel").addClass("success-panel");
-    } else {
-      $("#number_panel").removeClass("success-panel");
-    }
-    if (has_symbol == true) {
-      $("#symbol_panel").removeClass("failure-panel").addClass("success-panel");
-    } else {
-      $("#symbol_panel").removeClass("success-panel");
-    }
-    if (is_long_enough == true) {
-      $("#length_panel").removeClass("failure-panel").addClass("success-panel");
-    } else {
-      $("#length_panel").removeClass("success-panel");
+    // Change the classes for the character and input panels
+    change_class("#lower_case_panel", check_character(password, lower_cases));
+    change_class("#upper_case_panel", check_character(password, upper_cases));
+    change_class("#number_panel", check_character(password, numbers));
+    change_class("#symbol_panel", check_character(password, symbols));
+    change_class("#input_panel", passwords_match);
+  }
+
+  $scope.change_password_copy = function() {
+    var password = $scope.password;
+    var password_copy = $scope.password_copy;
+    if (password == password_copy) {
+      passwords_match = true;
     }
     if (passwords_match == true) {
-      // TODO: Set the passwords input panel to be green
+      $("#input_panel").removeClass("failure-panel").addClass("success-panel");
     } else {
-      // TODO: Set the passwords input panel to be default
+      $("#input_panel").removeClass("success-panel");
     }
   }
 
@@ -119,35 +116,27 @@ app.controller('checker_controller', function($scope, $location) {
     // Will handle the core of the password checking and route to next page
     var pass = true;
     if (has_lower_case != true) {
-      pass = false;
-      $("#lower_case_panel").addClass("failure-panel");
+      pass = false; $("#lower_case_panel").addClass("failure-panel");
     }
     if (has_upper_case != true) {
-      pass = false;
-      $("#upper_case_panel").addClass("failure-panel");
+      pass = false; $("#upper_case_panel").addClass("failure-panel");
     }
     if (has_number != true) {
-      pass = false;
-      $("#number_panel").addClass("failure-panel");
+      pass = false; $("#number_panel").addClass("failure-panel");
     }
     if (has_symbol != true) {
-      pass = false;
-      $("#symbol_panel").addClass("failure-panel");
+      pass = false; $("#symbol_panel").addClass("failure-panel");
     }
     if (is_long_enough != true) {
-      pass = false;
-      $('#length_panel').addClass("failure-panel");
+      pass = false; $('#length_panel').addClass("failure-panel");
     }
     if (passwords_match != true) {
-      pass = false;
-      // TODO: Set password input panel to be red
+      pass = false; $("#input_panel").addClass("failure-panel")
     }
     if (pass == true) {
       $location.path("/success");
     }
   }
-
-
 });
 
 
